527d5879f6206d338f4317a2ad17b71dc654ec61
Mbetania <betaniamarianag@gmail.com>
2023-02-18 01:29:26 -0300

    animate funciona desktop, section l102 se rompe mobile

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-    console.log('shown.bs.modal')
-  myInput.focus()
-})
-
-const startCard1 = document.querySelector('#start_card_1');
-const startCard2 = document.querySelector('#start_card_2');
-const startCard3 = document.querySelector('#start_card_3');
-
 function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
+  let rect = el.getBoundingClientRect();
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
@@ -19,18 +7,30 @@ function isElementInViewport(el) {
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
+let cards = document.querySelectorAll('.carta');
 
-function animateIfVisible(target) {
-  if (isElementInViewport(target)) {
-    target.classList.add('animate__backInLeft');
-  } else {
-    target.classList.remove('animate__backInLeft');
+function checkCards() {
+  for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    if (isElementInViewport(card)) {
+      card.classList.remove('hidden');
+    } else {
+      card.classList.add('hidden');
+    }
   }
 }
 
-animateIfVisible(startCard1);
-animateIfVisible(startCard2);
-animateIfVisible(startCard3);
-window.addEventListener('scroll', ()=>animateIfVisible(startCard1));
-window.addEventListener('scroll', ()=>animateIfVisible(startCard2));
-window.addEventListener('scroll', ()=>animateIfVisible(startCard3));
+// Ejecute la función en la carga de la página y cada vez que se desplaza la página
+
+window.addEventListener('scroll', checkCards);
+
+// const section = document.querySelector('#info-container');
+// const cartContainer = document.querySelector('.cart-container');
+
+// section.addEventListener('mouseover', () => {
+//   cartContainer.classList.add('animate__backInLeft');
+// });
+
+// section.addEventListener('mouseout', () => {
+//   cartContainer.classList.remove('animate__backInLeft');
+// });
